3e0ec03d2d7adf965e36e5c04e7ec2bf2c801e9d
StasZ2023 <159694096+StasZ2023@users.noreply.github.com>
2024-11-06 13:29:24 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+// import ReactDOM from "react-dom";
+import App from './components/app';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(<App />);
+
+// createRoot(<App />, document.getElementById("root"));
